c3547059d2257b13abccbc9c88048eb9abdee374
Kgothatso2 <kgothatso.m1997@gmail.com>
2024-04-25 16:13:29 +0200

    Keep every promise you make and only make promises you can keep

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,22 +1,23 @@
 function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        // Simulate API request using setTimeout
-        setTimeout(() => {
-            // Simulate successful response
-            const response = {
-                status: 200,
-                data: {
-                    message: "Success",
-                    // Other data received from API
-                }
-            };
-            resolve(response);
-            
-            // Simulate error response
-            // const error = new Error("API request failed");
-            // reject(error);
-        }, 1000); // Simulating 1 second delay
-    });
+  return new Promise((resolve) => {
+    // Simulate API request using setTimeout
+    setTimeout(() => {
+      // Simulate successful response
+      const response = {
+        status: 200,
+        data: {
+          message: 'Success', // Changed to single quotes
+          // Other data received from API
+        },
+      };
+      resolve(response);
+
+      // Simulate error response
+      // const error = new Error("API request failed");
+      // reject(error);
+    }, 1000); // Simulating 1 second delay
+  });
 }
 
 export default getResponseFromAPI;
+
