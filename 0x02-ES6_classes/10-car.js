09a1e627d7ed654210c6d51e7ce260910ccb0ef4
Kgothatso2 <kgothatso.m1997@gmail.com>
2024-04-25 14:23:41 +0200

    Vroom

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const { _brand, _motor, _color } = this;
+    return new Car(_brand, _motor, _color);
+  }
+}
+
+export default Car;
+
