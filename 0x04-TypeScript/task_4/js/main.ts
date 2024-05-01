a604c918669eb5587ccfb4f30fb520526650653e
Kgothatso2 <kgothatso.m1997@gmail.com>
2024-05-01 12:57:33 +0200

    Update task_4/js/main.ts

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,31 @@
+import { Subjects } from './subjects';
+
+// Export constants for subjects
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+// Create a Teacher object with experienceTeachingC = 10
+export const cTeacher: Subjects.Teacher = {
+  firstName: "John",
+  lastName: "Doe",
+  experienceTeachingC: 10,
+};
+
+// For Cpp subject
+console.log("Cpp:");
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// For Java subject
+console.log("\nJava:");
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// For React subject
+console.log("\nReact:");
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
